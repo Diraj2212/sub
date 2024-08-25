30172b74df0b6ca2becc11103f2e0549a5970800
Chintu <neelamdheerajkumar1122@gmail.com>
2024-08-25 13:38:32 +0530

    hi from sub

---
+++ sub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number: 50"
+
+# Ask user for second number
+echo "Enter second number: 20"
+
+# Perform subtraction
+diff=$((50 - 20))
+
+# Display result
+echo "Difference: $diff"
+
